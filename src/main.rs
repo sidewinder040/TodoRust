fe1b496fc0c9e9fb31d7937b103b28efe9d3a744
sidewinder040 <macevic040@hotmail.co.uk>
2020-08-18 23:22:33 +0100

    Added a Todo Struct to main - working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct TodoItem {
+    title: String,
+    description: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let todo1 = TodoItem {
+        title: String::from("Go to shops"),
+        description:String::from("I need to buy milk")
+    };
+    display(todo1);
+}
+
+fn display(todo: TodoItem) {
+   println!("Title: {}", todo.title);
+   println!("Description: {}", todo.description);
 }
